866faa06085dc894a68bb882951b7539123fe1e0
がる <garuneko@gmail.com>
2026-02-25 21:34:56 +0900

    Update notarize.js

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -8,15 +8,15 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
   const appPath = path.join(appOutDir, `${appName}.app`);
 
-  console.log(`\n--- Notarization Start: ${appName} ---`);
+  console.log(`\n--- Custom Notarization Start: ${appName} ---`);
 
-  // electron-builderにバレないように独自の環境変数名「MY_」で受け取る
+  // builderのバグを避けるため、独自の環境変数 MY_ から読み込む
   const apiKeyPath = process.env.MY_APPLE_API_KEY;
   const apiKeyId = process.env.MY_APPLE_API_KEY_ID;
   const apiIssuer = process.env.MY_APPLE_API_ISSUER;
 
   if (!apiKeyPath || !apiKeyId || !apiIssuer) {
-    console.log("Skipping notarization: Missing API keys.");
+    console.log("Skipping notarization: Missing MY_ environment variables.");
     return;
   }
 
@@ -28,9 +28,9 @@ exports.default = async function notarizing(context) {
       appleApiKeyId: apiKeyId,
       appleApiIssuer: apiIssuer
     });
-    console.log(`--- Notarization Successful! ---`);
+    console.log(`--- Custom Notarization Successful! ---`);
   } catch (error) {
-    console.error('Notarization failed:', error);
+    console.error('Custom notarization failed:', error);
     throw error;
   }
 };
